c61eaf8b68a153ffd1f8fa954b0879e4a4a73fc5
Islam Bin ElHassan <108907313+ai1islam1411@users.noreply.github.com>
2023-04-18 03:38:21 +0200

    Update light.ino

--- light.ino
+++ light.ino
@@ -1,38 +1,34 @@
-#include <ESP8266WiFi.h>
+/* Alsalam Ealaykum Warahmat Allah Wabarakatuh Dear Insan Technology Students */
 
+#include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
 
 WiFiClient client;
-
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(01000098000);
 ThingSpeak.begin(client);
-
-pinMode(16, OUTPUT);
+pinMode(27, OUTPUT);
   WiFi.disconnect();
-   WiFi.begin("<name of the network>","<password>");
+   WiFi.begin("<Name of The Network>","<Password>");
   while ((!(WiFi.status() == WL_CONNECTED))){
     delay(300);
     Serial.println("................");
-
   }
   Serial.println("Connected!");
   Serial.println((WiFi.localIP().toString()));
 
 }
 
-
 void loop()
 {
 
-    a = (ThingSpeak.readIntField("<Enter the channel number in int format>",1,"<Thingspeak Read API key>"));
+    a = (ThingSpeak.readIntField("<Enter The Channel Number In Int Format>",1,"<Thingspeak Read API key>"));
     Serial.println(a);
     if (a == "on") {
-      digitalWrite(16,LOW);
+      digitalWrite(27,LOW);
 
     } else if (a == "off") {
-      digitalWrite(16,HIGH);
+      digitalWrite(27,HIGH);
     }
-
 }
